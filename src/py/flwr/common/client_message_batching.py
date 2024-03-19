46c4faa808e3af0d300a664969acbfa734c0c829
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-03-19 18:36:07 +0000

    Making sure to send exact max size gRPC messages

--- src/py/flwr/common/client_message_batching.py
+++ src/py/flwr/common/client_message_batching.py
@@ -3,45 +3,40 @@ from typing import List, Iterator
 from flwr.proto.transport_pb2 import ClientMessageChunk, ClientMessage
 
 
+CLIENT_MESSAGE_CHUNK_HEADER_SIZE = 2
+
+
 def get_client_message_chunk_size() -> int:
-    return len(ClientMessageChunk(client_message_chunk=b"").SerializeToString()) + 16
+    return CLIENT_MESSAGE_CHUNK_HEADER_SIZE
 
 
 def batch_client_message(client_message: ClientMessage, chunk_size: int) -> List[ClientMessageChunk]:
-    print(f"Sending chunk of size {chunk_size} to")
+    chunk_size -= get_client_message_chunk_size()
+
     client_message_bytes = client_message.SerializeToString()
     client_message_bytes_len = len(client_message_bytes)
 
     num_chunks = ceil(client_message_bytes_len / chunk_size)
     client_message_chunks: List[ClientMessageChunk] = []
-    for chunk_num in range(1, num_chunks + 1):
+    for chunk_num in range(num_chunks):
         chunk_bytes = client_message_bytes[
-                      (chunk_num - 1) * chunk_size:
-                      min(chunk_num * chunk_size, client_message_bytes_len)
-                      ]
+                        chunk_num * chunk_size:
+                        min((chunk_num + 1) * chunk_size, client_message_bytes_len)
+        ]
 
-        cm = ClientMessageChunk(
-            batch_number=chunk_num,
-            num_batches=num_chunks,
-            client_message_chunk=chunk_bytes
-        )
+        client_message_chunks.append(ClientMessageChunk(client_message_chunk=chunk_bytes))
 
-        print(f"cm size = {len(cm.SerializeToString())}")
-
-        client_message_chunks.append(cm)
-
+    client_message_chunks.append(ClientMessageChunk())
     return client_message_chunks
 
 
 def get_client_message_from_batches(client_message_batches: Iterator[ClientMessageChunk]) -> ClientMessage:
-    chunk_num = 0
     client_message_bytes = b""
     while True:
-        chunk_num += 1
         client_message_batch: ClientMessageChunk = next(client_message_batches)
-        client_message_bytes += client_message_batch.client_message_chunk
-        if chunk_num == client_message_batch.num_batches:
+        if len(client_message_batch.client_message_chunk) == 0:
             break
+        client_message_bytes += client_message_batch.client_message_chunk
 
     client_message = ClientMessage()
     client_message.ParseFromString(client_message_bytes)
--- src/py/flwr/common/server_message_batching.py
+++ src/py/flwr/common/server_message_batching.py
@@ -4,35 +4,41 @@ from typing import List, Iterator
 from flwr.proto.transport_pb2 import ServerMessage, ServerMessageChunk
 
 
+SERVER_MESSAGE_CHUNK_HEADER_SIZE = 2
+
+
+def get_server_message_chunk_size() -> int:
+    return SERVER_MESSAGE_CHUNK_HEADER_SIZE
+
+
 def batch_server_message(server_message: ServerMessage, chunk_size: int) -> List[ServerMessageChunk]:
+    chunk_size -= get_server_message_chunk_size()
+
     server_message_bytes = server_message.SerializeToString()
     server_message_bytes_len = len(server_message_bytes)
 
     num_chunks = ceil(server_message_bytes_len / chunk_size)
     server_message_chunks: List[ServerMessageChunk] = []
-    for chunk_num in range(1, num_chunks + 1):
+    for chunk_num in range(num_chunks):
         chunk_bytes = server_message_bytes[
-                      (chunk_num - 1) * chunk_size:
-                      min(chunk_num * chunk_size, server_message_bytes_len)
+                      chunk_num * chunk_size:
+                      min((chunk_num + 1) * chunk_size, server_message_bytes_len)
         ]
         server_message_chunks.append(ServerMessageChunk(
-            batch_number=chunk_num,
-            num_batches=num_chunks,
             server_message_chunk=chunk_bytes
         ))
 
+    server_message_chunks.append(ServerMessageChunk())
     return server_message_chunks
 
 
 def get_server_message_from_batches(server_message_batches: Iterator[ServerMessageChunk]) -> ServerMessage:
-    chunk_num = 0
     server_message_bytes = b""
     while True:
-        chunk_num += 1
         server_message_batch: ServerMessageChunk = next(server_message_batches)
-        server_message_bytes += server_message_batch.server_message_chunk
-        if chunk_num == server_message_batch.num_batches:
+        if len(server_message_batch.server_message_chunk) == 0:
             break
+        server_message_bytes += server_message_batch.server_message_chunk
 
     server_message = ServerMessage()
     server_message.ParseFromString(server_message_bytes)
